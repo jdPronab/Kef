81e6f8ed644403502c58af09516e2f4afa41e526
jdPronab <jdpronab@gmail.com>
2023-01-03 12:55:41 +0530

    scoring system implemented

--- main.py
+++ main.py
@@ -12,37 +12,37 @@ import random
 
 
 class Emoji(Image):
-    root = ObjectProperty(None)
-    velocity_x = NumericProperty(0)
-    velocity_y = NumericProperty(0)
-    velocity = ReferenceListProperty(velocity_x, velocity_y)
+	root = ObjectProperty(None)
+	velocity_x = NumericProperty(0)
+	velocity_y = NumericProperty(0)
+	velocity = ReferenceListProperty(velocity_x, velocity_y)
 
-    def spawn(self):
-        '''TODO:// spawn mode ['iruption', 'circle']'''
+	def __init__(self, recognition, *args, **kwargs):
+		super().__init__(**kwargs)
+		self.recognition = recognition
+
+	def spawn(self):
+		'''TODO:// spawn mode ['eruption', 'circle']'''
         #self.pos = Vector(*self.kefmain.center)
+		self.pos =  Vector(*self.parent.center)
 
-        self.pos =  Vector(*self.parent.center)
-
-    def move(self):
-        try:
-            self.pos = Vector(*self.velocity) + self.pos
-        except Exception as e:
-            print(e)
-            print(self.pos)
-            print(self.velocity)
+	def move(self):
+		try:
+			self.pos = Vector(*self.velocity) + self.pos
+		except Exception as e:
+			print(e)
+			print(self.pos)
+			print(self.velocity)
 
 class KefMain(FloatLayout):
     eggplant = ObjectProperty(None)
     middle_finger = ObjectProperty(None)
+    kscore = NumericProperty(0)
+    fscore = NumericProperty(0)
 
-    '''
-    def __init__(self, **kwargs):
-        super().__init__(self, **kwargs)
-        self.sprites = []
-    '''
-    direction = ListProperty([1, -1])
     def kspawn(self):
         ep  = Emoji(
+			'eggplant',
             #velocity_x = random.choice(self.direction), #randint(-180, 180),
             #velocity_y = random.choice(self.direction), #randint(-180, 180),
             velocity_x = random.randint(-10, 10) * random.random(),
@@ -53,6 +53,7 @@ class KefMain(FloatLayout):
 
     def fspawn(self):
         mf = Emoji(
+			'middle-finger',
             #velocity_x = random.choice(self.direction), #randint(-180, 180),
             #velocity_y = random.choice(self.direction), #randint(-180, 180),
             velocity_x = random.randint(-10, 10) * random.random(),
@@ -62,30 +63,19 @@ class KefMain(FloatLayout):
         self.add_widget(mf)
 
     def update(self, dt):
-        if len(self.children) > 2:
+        if len(self.children) > 4:
             half_window_width = Window.width / 2     # GET HALF OF WINDOWS's WIDTH
             half_window_height = Window.height / 2 # GET HALF OF WINDOWS's WIDTH
-            for child in self.children[:-2]:
-                #if child.y > Window.height - half_window_height or child.y < 0 - half_window_height or child.x - half_window_width > Window.width or  child.x < 0 - half_window_width:
+            for child in self.children[:-4]:
                 if child.y > Window.height - half_window_height or child.y < 0 - half_window_height or child.x > Window.width - half_window_width or  child.x < 0 - half_window_width:
+                    if child.recognition == 'eggplant':
+                        self.kscore += 1
+                    else:
+                        self.fscore += 1
                     self.remove_widget(child)
                 child.x += child.velocity_x
                 child.y += child.velocity_y
                 child.move()
-
-                '''
-            for child in self.children[:-2]:
-                #if child.y > selfright and child.right < self.y and child.x > self.top and  child.top < self.x:
-                if child.y > Window.width or child.y < 0 or child.x > Window.height or  child.x < 0:
-                    self.remove_widget(child)
-            for child in  self.children[:-2]:
-                #child.velocity_x += child.velocity_x
-                #child.velocity_y += child.velocity_y
-                child.x += child.velocity_x
-                child.y += child.velocity_y
-                child.move()
-                '''
-
 
 class KefApp(App):
     def build(self):
